d3fab6fc13a08b8c8bb2330c0ab4fc9cab0e2948
tania.dev <taniachristian01@gmail.com>
2025-06-04 01:45:31 +0800

    Refactor title

--- jobs-board-app/src/app/modules/layout/layout.module.ts
+++ jobs-board-app/src/app/modules/layout/layout.module.ts
@@ -9,14 +9,12 @@ import { JobDetailsComponent } from "../job/job-details/job-details.component";
 import { JobService } from "../services/job-services";
 import { ApplicantService } from "../services/applicant.service";
 import { JobDashboardComponent } from "../job/job-dashboard/job-dashboard.component";
-import { BrowserModule } from "@angular/platform-browser";
 import { LayoutRoutingModule } from "./layout-routing.module";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { NumberDirective } from "src/app/shared/directives/numbers-only.directive";
 import { FilterTextPipe } from "src/app/shared/pipe/filter-pipe.pipe";
 import { PostedDatePipe } from "src/app/shared/pipe/postedDate-pipe";
 import { FilterDataPipe } from "src/shared/pipe/filterdata.pipe";
-import { HomeComponent } from "../home/home.component";
 @NgModule({
   imports: [
     CommonModule,
@@ -28,7 +26,6 @@ import { HomeComponent } from "../home/home.component";
   ],
   declarations: [
     LayoutComponent,
-    HomeComponent,
     JobDashboardComponent,
     CreateJobComponent,
     EditJobComponent,
